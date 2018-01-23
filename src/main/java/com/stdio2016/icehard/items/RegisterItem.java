198ece3998c9c44447f024ce7bcefbe568497a84
stdio2016 <randomfrank8@gmail.com>
2018-01-23 17:14:11 +0800

    add copper

--- src/main/java/com/stdio2016/icehard/items/RegisterItem.java
+++ src/main/java/com/stdio2016/icehard/items/RegisterItem.java
@@ -4,6 +4,7 @@ import net.minecraft.item.Item;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.registries.IForgeRegistry;
 
 import java.util.ArrayList;
@@ -14,11 +15,14 @@ import java.util.List;
  */
 
 public class RegisterItem {
-    public static MyItem copperNugget;
+    public static MyItem copperNugget, copper;
     public static List<Item> items = new ArrayList<>();
 
     public static void preInit(FMLPreInitializationEvent event) {
         items.add(ItemEnergyPile.item);
+        copper = new MyItem("copper");
+        OreDictionary.registerOre("ingotCopper", copper);
+        items.add(copper);
         copperNugget = new MyItem("copper_nugget");
         items.add(copperNugget);
     }
