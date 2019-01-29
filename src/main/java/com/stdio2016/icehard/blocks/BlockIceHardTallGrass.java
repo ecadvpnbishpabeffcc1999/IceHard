400ff9bf8b5fb9110d663cf8c30420fa344d8759
stdio2016 <randomfrank8@gmail.com>
2019-01-29 19:53:01 +0800

    get seed from hitting grass

--- src/main/java/com/stdio2016/icehard/blocks/BlockIceHardTallGrass.java
+++ src/main/java/com/stdio2016/icehard/blocks/BlockIceHardTallGrass.java
@@ -1,17 +1,13 @@
 package com.stdio2016.icehard.blocks;
 
 import com.stdio2016.icehard.IceHardMod;
+import com.stdio2016.icehard.items.RegisterItem;
 import net.minecraft.block.BlockBush;
-import net.minecraft.block.BlockLog;
 import net.minecraft.block.BlockTallGrass;
 import net.minecraft.block.SoundType;
-import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.IProperty;
-import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
@@ -23,11 +19,8 @@ import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import net.minecraftforge.common.EnumPlantType;
-import net.minecraftforge.common.IPlantable;
 import net.minecraftforge.common.IShearable;
 
-import javax.annotation.Nullable;
 import java.util.Random;
 
 /**
@@ -71,7 +64,9 @@ public class BlockIceHardTallGrass extends BlockBush implements IShearable, IBlo
     @Override
     public void getDrops(NonNullList<ItemStack> list, IBlockAccess  world, BlockPos pos, IBlockState blockstate, int fortune) {
         if (RANDOM.nextInt(8) == 1) {
-            list.add(new ItemStack(this.item) /* TODO: seed */);
+            int r = RANDOM.nextInt(2);
+            if (r == 0) list.add(new ItemStack(RegisterItem.HaicerdSeed));
+            else if (r == 1) list.add(new ItemStack(RegisterItem.HirecadSeed));
         }
     }
 
--- src/main/java/com/stdio2016/icehard/items/ItemEdibleIceHard.java
+++ src/main/java/com/stdio2016/icehard/items/ItemEdibleIceHard.java
@@ -1,21 +1,9 @@
 package com.stdio2016.icehard.items;
 
 import com.stdio2016.icehard.IceHardMod;
-import com.stdio2016.icehard.blocks.BlockIceHard;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.init.MobEffects;
-import net.minecraft.init.SoundEvents;
-import net.minecraft.item.EnumAction;
-import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
-import net.minecraft.potion.PotionEffect;
-import net.minecraft.util.*;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.RayTraceResult;
-import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.World;
 
 /**
